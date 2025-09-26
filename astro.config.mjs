4c09dc6c9fbfed236822e0f368cc6882cdd4a46e
SOUBOURA DJAFSIA Ebenezer <105481206+DJAFSIA@users.noreply.github.com>
2025-09-26 22:51:39 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site : 'https://DJAFSIA.github.io' ,
+  base: 'PortfolioEben' ,
+  trailingSlash: 'always' ,
   integrations: [tailwind()],
 });
