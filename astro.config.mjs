3e48e16d08f7002bbcac7738f245352660cf0562
Ebenezer Djafsia <ebenezerdjafsiasb@gmail.com>
2025-09-30 16:52:23 +0200

    Ajout d'un Workflow de déploiement GitHub Pages et mise à jour de la configuration du site

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,10 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://djafsia.github.io',
+  base: 'PortfolioEben' ,
+  trailingSlash: 'always',
   integrations: [tailwind()],
 });
+
+
